b5024926851e2fef1266ef239c2e23898da88bfe
mornemira <mornemira@yandex.ru>
2021-03-21 22:02:53 +0300

    add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
 import {
@@ -7,13 +7,17 @@ import {
   IconRegistry,
   List,
   Layout,
+  TabView,
+  Tab,
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Search, TrainingCard } from './components';
 
-const array = new Array(30).fill({ text: 'some item', time: '1:44' });
+const array = new Array(15).fill({ text: 'some item', time: '1:44' });
 
 const App = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
@@ -23,13 +27,23 @@ const App = () => {
             <Layout>
               <Search />
             </Layout>
-            <Layout>
-              <Text category='h1'>Tabs</Text>
-            </Layout>
-            <List
-              data={array}
-              renderItem={(props) => <TrainingCard {...props} />}
-            />
+            <TabView
+              selectedIndex={selectedIndex}
+              onSelect={(index) => setSelectedIndex(index)}>
+              <Tab title='Тренировки'>
+                <Layout>
+                  <List
+                    data={array}
+                    renderItem={(props) => <TrainingCard {...props} />}
+                  />
+                </Layout>
+              </Tab>
+              <Tab title='Сеты'>
+                <Layout>
+                  <Text category='h5'>Сеты</Text>
+                </Layout>
+              </Tab>
+            </TabView>
           </Layout>
         </SafeAreaView>
       </ApplicationProvider>
