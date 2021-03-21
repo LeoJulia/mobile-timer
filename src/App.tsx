9c5bccd810165d2e25b47b9f591cc432b1d7de65
mornemira <mornemira@yandex.ru>
2021-03-21 22:08:10 +0300

    move tabs to module

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
 import {
   ApplicationProvider,
-  Text,
   IconRegistry,
-  List,
   Layout,
-  TabView,
-  Tab,
 } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { Search, TrainingCard } from './components';
+import { Search } from './components';
+import { Tabs } from './modules';
 
-const array = new Array(15).fill({ text: 'some item', time: '1:44' });
-
-const App = () => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  return (
-    <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <SafeAreaView>
-          <Layout style={styles.container}>
-            <Layout>
-              <Search />
-            </Layout>
-            <TabView
-              selectedIndex={selectedIndex}
-              onSelect={(index) => setSelectedIndex(index)}>
-              <Tab title='Тренировки'>
-                <Layout>
-                  <List
-                    data={array}
-                    renderItem={(props) => <TrainingCard {...props} />}
-                  />
-                </Layout>
-              </Tab>
-              <Tab title='Сеты'>
-                <Layout>
-                  <Text category='h5'>Сеты</Text>
-                </Layout>
-              </Tab>
-            </TabView>
+const App = () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaView>
+        <Layout style={styles.container}>
+          <Layout>
+            <Search />
           </Layout>
-        </SafeAreaView>
-      </ApplicationProvider>
-    </>
-  );
-};
+          <Tabs />
+        </Layout>
+      </SafeAreaView>
+    </ApplicationProvider>
+  </>
+);
 
 const styles = StyleSheet.create({
   container: {
